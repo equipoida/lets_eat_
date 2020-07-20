85e0774c1b329593c2338d2dadabeafc2aa4526a
10000bit <khb990507@naver.com>
2020-07-20 13:51:56 +0900

    별점 4개이상일 때 추천 코드 기본 틀

--- app/src/main/java/com/example/lets_eat/Recommendation.java
+++ app/src/main/java/com/example/lets_eat/Recommendation.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RatingBar;
 import android.widget.Toast;
 
 public class Recommendation extends AppCompatActivity {
@@ -12,7 +13,20 @@ public class Recommendation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommendation);
+
+        //if(R.id.ratingBar)
+
+        final RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                if(ratingBar.getRating()>=4) { //별점이 4개 이상일 때
+
+                }
+            }
+        });
     }
+
 
 
     public void mOnClick(View view) {

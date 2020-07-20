507e54a51e45cc05024727c4e2d5667499695433
hahayoungyou <whgkdud0501@naver.com>
2020-07-20 15:08:15 +0900

    교직원식당 메뉴

--- app/src/main/java/com/example/lets_eat/SubActivity.java
+++ app/src/main/java/com/example/lets_eat/SubActivity.java
@@ -5,18 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.lets_eat.databinding.ActivityMainBinding;
 import com.example.lets_eat.databinding.ActivitySubBinding;
 
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -24,6 +37,8 @@ import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class SubActivity extends AppCompatActivity {
     private ActivitySubBinding binding;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,9 +50,53 @@ public class SubActivity extends AppCompatActivity {
         String imageUrl = "https://www.hansung.ac.kr/portlet-repositories/fckeditor/images/7qHyuRAiKcc=/1595204995118.png";
         Glide.with(this).load(imageUrl).into(imgView);
 
+        binding.textView7.setMovementMethod(new ScrollingMovementMethod()); //스
 
+                JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+                jsoupAsyncTask.execute();
 
     }
+
+    private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
+        private String htmlPageUrl = "https://www.hansung.ac.kr/web/www/life_03_01_t2"; //파싱할 홈페이지의 URL주소
+        private String htmlContentInStringFormat="";
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(htmlPageUrl).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            //테스트1
+            Elements titles= doc.select("td");
+
+            for(Element e: titles){
+                System.out.println(e.text());
+                htmlContentInStringFormat += e.text().trim() + "\n";
+
+                String humi = e.text();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            binding.textView7.setText(htmlContentInStringFormat);
+        }
+    }
+
+
 
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();

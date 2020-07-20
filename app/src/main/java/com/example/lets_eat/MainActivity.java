914b2fef56a4fb33557e91c016c2e44a4ae55ac4
hahayoungyou <whgkdud0501@naver.com>
2020-07-20 11:12:50 +0900

    ui연결 부분

--- app/src/main/java/com/example/lets_eat/MainActivity.java
+++ app/src/main/java/com/example/lets_eat/MainActivity.java
@@ -22,12 +22,12 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        final Intent intent = new Intent(this, Rating.class);
+        final Intent intent = new Intent(this, SubActivity.class);
         final Intent intent2 = new Intent(this, Join.class);
 
         binding.login.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                //로그인 버튼을 누르면, 점수 매기는 액티비티 실행
+                //로그인 버튼을 누르면, 학식메뉴 나오게하기
                 //이곳에서 로그인 정보 확인하는 코드 필요
                 startActivity(intent);
             }
--- app/src/main/java/com/example/lets_eat/SubActivity.java
+++ app/src/main/java/com/example/lets_eat/SubActivity.java
@@ -2,6 +2,7 @@ package com.example.lets_eat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,6 +15,7 @@ public class SubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -24,18 +26,24 @@ public class SubActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        final Intent intent3 = new Intent(this, notification_list.class);
+        final Intent intent4 = new Intent(this, Rating.class);
         switch (item.getItemId()) {
             case R.id.item1:
                 Toast.makeText(this, "추천메뉴", Toast.LENGTH_SHORT).show();
+
                 return true;
             case R.id.item2:
                 Toast.makeText(this, "혼잡도", Toast.LENGTH_SHORT).show();
+
                 return true;
             case R.id.item3:
                 Toast.makeText(this, "알림", Toast.LENGTH_SHORT).show();
+                startActivity(intent3);
                 return true;
             case R.id.item4:
                 Toast.makeText(this, "리뷰", Toast.LENGTH_SHORT).show();
+                startActivity(intent4);
                 return true;
 
         }

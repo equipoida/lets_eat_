428fe2ff8fd53262c48b75f36f446f16e1bf6508
10000bit <khb990507@naver.com>
2020-07-21 12:30:26 +0900

    회원가입 firebase에 정보 올리는 틀

--- app/src/main/java/com/example/lets_eat/Join.java
+++ app/src/main/java/com/example/lets_eat/Join.java
@@ -1,29 +1,41 @@
 package com.example.lets_eat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.lets_eat.databinding.ActivityJoinBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.List;
 
 public class Join extends AppCompatActivity {
     private ActivityJoinBinding binding;
-
+    private FirebaseAuth mAuth;
+    private static final String TAG = "signUpActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityJoinBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         final Intent intent = new Intent(this, MainActivity.class);
 
@@ -37,5 +49,53 @@ public class Join extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //pdateUI(currentUser);
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.signUp:
+                    signUp();
+                    break;
+            }
+        }
+    };
+
+    private void signUp() {
+        String email = ((EditText) findViewById(R.id.idText)).getText().toString();
+        String password = ((EditText) findViewById(R.id.passwordText)).getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user); //UI
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            /*
+                            //UI
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                             */
+                        }
+
+                        // ...
+                    }
+                });
     }
 }

ae7d3c6d9cb61d4e268a9121509c62ad29c2ecb2
hahayoungyou <whgkdud0501@naver.com>
2020-07-23 17:11:53 +0900

    자동로그인, 로그아웃

--- app/src/main/java/com/example/lets_eat/Person.java
+++ app/src/main/java/com/example/lets_eat/Person.java
@@ -1,14 +1,39 @@
 package com.example.lets_eat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.lets_eat.databinding.ActivityMainBinding;
+import com.example.lets_eat.databinding.ActivityPersonBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Person extends AppCompatActivity {
+    private ActivityPersonBinding binding;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_person);
+        binding = ActivityPersonBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        final Intent intent9 = new Intent(this, MainActivity.class);
+        binding.btnLogout.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        FirebaseAuth.getInstance().signOut();
+startActivity(intent9);
+                    }
+                }
+        );
     }
+
 }

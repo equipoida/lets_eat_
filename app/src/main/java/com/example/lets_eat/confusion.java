091b6714b3a5e0ded757893a153d1ebf3f8de215
hahayoungyou <whgkdud0501@naver.com>
2020-07-28 15:37:42 +0900

    한줄 건의함

--- app/src/main/java/com/example/lets_eat/confusion.java
+++ app/src/main/java/com/example/lets_eat/confusion.java
@@ -2,12 +2,18 @@
 package com.example.lets_eat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.lets_eat.databinding.ActivityConfusionBinding;
+
+import java.util.ArrayList;
 
 public class confusion extends AppCompatActivity {
     private ActivityConfusionBinding mBinding;
@@ -16,11 +22,24 @@ public class confusion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mBinding= ActivityConfusionBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
-        ProgressBar bar1 = mBinding.progressBar1;
-        ProgressBar bar2 = mBinding.progressBar2;
+        // 빈 데이터 리스트 생성.
+        final ArrayList<String> items = new ArrayList<String>() ;
+        // ArrayAdapter 생성. 아이템 View를 선택(single choice)가능하도록 만듦.
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items) ;
 
-        bar1.setProgress(50);
-        bar2.setProgress(50);
+        // listview 생성 및 adapter 지정.
+        final ListView listview = (ListView) findViewById(R.id.listview) ;
+        listview.setAdapter(adapter) ;
 
+        mBinding.button.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+                // 아이템 추가.
+                items.add(mBinding.editTextTextPersonName.getText().toString());
+
+                // listview 갱신
+                adapter.notifyDataSetChanged();
+            }
+        }) ;
     }
 }

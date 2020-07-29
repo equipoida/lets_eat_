cb187b39eb7756cb5f3dadb5d39034f8b7a88879
10000bit <khb990507@naver.com>
2020-07-29 15:24:08 +0900

    리스트뷰를 리니어레이아웃으로 변경

--- app/src/main/java/com/example/lets_eat/Recommendation.java
+++ app/src/main/java/com/example/lets_eat/Recommendation.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.Toast;
@@ -42,8 +43,8 @@ public class Recommendation extends AppCompatActivity {
 
         // ArrayAdapter 생성. 아이템 View를 선택(single choice)가능하도록 만듦.
         final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items) ;
-        final ListView listview = (ListView) findViewById(R.id.listview) ;
-        listview.setAdapter(adapter);
+        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout) ;
+        //linearLayout.setAdapter(adapter);
 
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -58,7 +59,7 @@ public class Recommendation extends AppCompatActivity {
                 // 클래스 모델이 필요?
                 for (DataSnapshot user : dataSnapshot.getChildren()) {
                     //MyFiles filename = (MyFiles) fileSnapshot.getValue(MyFiles.class);
-                    // review 안에 있는 user 안의 review를 listview에 넣기
+                    // review 안에 있는 user 안의 review를 linearlayout에 넣기
                     String str = user.child("review").getValue(String.class);
                     Log.i("TAG: value is ", str);
                     items.add(str);

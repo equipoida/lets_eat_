5a7dbb71b0d8e45bdc4b0209dc9ee1a93036f44d
hahayoungyou <whgkdud0501@naver.com>
2020-07-30 17:54:05 +0900

    알림 실패, 파싱만 해옴

--- app/src/main/java/com/example/lets_eat/notification_list.java
+++ app/src/main/java/com/example/lets_eat/notification_list.java
@@ -3,27 +3,20 @@ package com.example.lets_eat;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.PopupMenu;
-import android.widget.ProgressBar;
-import android.widget.RatingBar;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.lets_eat.databinding.ActivityConfusionBinding;
+import com.bumptech.glide.Glide;
 import com.example.lets_eat.databinding.ActivityNotificationListBinding;
-import com.example.lets_eat.databinding.ActivityRatingBinding;
+import com.example.lets_eat.databinding.ActivitySubBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -41,62 +34,28 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-public class notification_list extends AppCompatActivity {
-    private ActivityNotificationListBinding mBinding;
-    private FirebaseAuth mAuth;
-    FirebaseDatabase rootNode;
-    DatabaseReference mDatabase;
+import uk.co.senab.photoview.PhotoViewAttacher;
 
+public class notification_list extends AppCompatActivity {
+    private static ActivityNotificationListBinding binding;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mBinding = ActivityNotificationListBinding.inflate(getLayoutInflater());
-        ArrayList<String> items = new ArrayList<String>();
-        // ArrayAdapter 생성. 아이템 View를 선택(single choice)가능하도록 만듦.
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_2, items);
-        setContentView(mBinding.getRoot());
-        // 빈 데이터 리스트 생성.
+        binding = ActivityNotificationListBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        final ListView listview = (ListView) findViewById(R.id.listview);
-        listview.setAdapter(adapter);
 
-        JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+  JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
         jsoupAsyncTask.execute();
 
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference databaseRef = database.getReference("notification");
-
-        // Read from the database
-        databaseRef.child("notification").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                adapter.clear();
-                // 클래스 모델이 필요?
-                for (DataSnapshot notification : dataSnapshot.getChildren()) {
-                    //MyFiles filename = (MyFiles) fileSnapshot.getValue(MyFiles.class);
-                    //하위키들의 value를 어떻게 가져오느냐???
-                    String str = notification.child("notification").getValue(String.class);
-                    Log.i("TAG: value is ", str);
-                    items.add(str);
-                }
-               // adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        // listview 생성 및 adapter 지정.
     }
 
-   class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
+    static class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
         private String htmlPageUrl = "https://www.hansung.ac.kr/web/www/life_03_01_t1"; //파싱할 홈페이지의 URL주소
-        private String htmlContentInStringFormats = "";
+        private String htmlContentInStringFormat = "";
         Date currentTime = Calendar.getInstance().getTime();
-        SimpleDateFormat weekdayFormat = new SimpleDateFormat("EE", Locale.getDefault());
+        SimpleDateFormat weekdayFormat = new SimpleDateFormat("EE", Locale.ENGLISH);
 
         @Override
         protected void onPreExecute() {
@@ -112,18 +71,24 @@ public class notification_list extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            htmlContentInStringFormats =doc.select("span").get(204).text();
-            items.add(htmlContentInStringFormats);
-            adapter.notifyDataSetChanged();
+
+
+            //테스트1
+            // Elements titles = doc.select("td"); //2: 월요일 3: 화요일
+
+            // for (Element e : titles) {
+            String weekday = weekdayFormat.format(currentTime);
+            htmlContentInStringFormat = doc.select("span").get(204).text();
+Log.i("TAG: value is ",htmlContentInStringFormat);
             return null;
+
+
         }
+
         @Override
         protected void onPostExecute(Void result) {
 
-
-
-
+            binding.textView4.setText(htmlContentInStringFormat);
         }
     }
 }
-

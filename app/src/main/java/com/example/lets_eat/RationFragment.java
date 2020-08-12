649d09122772fd1e46d8c89426bb0cad1018c87b
yewonkim <ywon9909@naver.com>
2020-08-12 12:15:43 +0900

    리뷰, 한줄건의 프래그먼트

--- app/src/main/java/com/example/lets_eat/RationFragment.java
+++ app/src/main/java/com/example/lets_eat/RationFragment.java
@@ -5,8 +5,18 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,7 +33,11 @@ public class RationFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    RatingBar mstar;
+    EditText mreview;
+    Button mmenuname;
+    FirebaseDatabase rootNode;
+    DatabaseReference mDatabase;
     public RationFragment() {
         // Required empty public constructor
     }
@@ -59,6 +73,64 @@ public class RationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ration, container, false);
+        View view =inflater.inflate(R.layout.fragment_ration, container, false);
+        mstar = (RatingBar)view.findViewById(R.id.ratingBar);
+        mreview = (EditText)view.findViewById(R.id.editText);
+        mmenuname = (Button)view.findViewById(R.id.menuchoice);
+       // final Button menuchoice = (Button)view.findViewById(R.id.menuchoice);
+        final Button submit = (Button)view.findViewById(R.id.submit);
+        mmenuname.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(getContext(), view);
+                MenuInflater inflater = getActivity().getMenuInflater();
+                inflater.inflate(R.menu.popup, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        mmenuname.setText(menuItem.getTitle());
+                        return false;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rootNode = FirebaseDatabase.getInstance();
+                mDatabase = rootNode.getReference("review");
+
+
+                //값을 데이터베이스에 보내기
+                String menuname = mmenuname.getText().toString();
+                String review = mreview.getText().toString();
+                String star = String.valueOf(mstar.getRating());
+
+                Userhelper helperclass = new Userhelper(menuname, review, star);
+
+                //review 안의 user
+                //user 안의 push()로 만들어준 랜덤한 문자(개별적인 걸 위함)
+                //그 안에 menuname, review, star의 값을 받아줌.
+                mDatabase.child("user").push().setValue(helperclass);
+
+                /*
+
+                // 아이템 추가.
+                items.add(mBinding.menuchoice.getText().toString());
+                items.add(String.valueOf(mBinding.ratingBar.getRating()));
+                //Recommendation의 listview 갱신
+                //adapter.notifyDataSetChanged();
+
+
+                 */
+
+                Toast.makeText(getContext(), "리뷰가 저장됨", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        return view;
     }
 }

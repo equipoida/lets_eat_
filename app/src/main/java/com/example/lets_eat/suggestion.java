eb7227a32f06312ccd1c4e4657c1ad6f8d87bee0
yewonkim <ywon9909@naver.com>
2020-08-12 12:15:43 +0900

    리뷰, 한줄건의 프래그먼트

--- app/src/main/java/com/example/lets_eat/suggestion.java
+++ app/src/main/java/com/example/lets_eat/suggestion.java
@@ -2,11 +2,28 @@ package com.example.lets_eat;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,10 +36,19 @@ public class suggestion extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private FirebaseAuth mAuth;
+    FirebaseDatabase rootNode;
+    DatabaseReference mDatabase;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Button btn;
+    private EditText editText;
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
 
     public suggestion() {
         // Required empty public constructor
@@ -59,6 +85,57 @@ public class suggestion extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_suggestion, container, false);
+        View view =inflater.inflate(R.layout.fragment_suggestion, container, false);
+        btn=(Button)view.findViewById(R.id.button);
+        editText=(EditText)view.findViewById(R.id.editTextTextPersonName);
+        final ArrayList<String> items = new ArrayList<String>() ;
+        // ArrayAdapter 생성. 아이템 View를 선택(single choice)가능하도록 만듦.
+        final ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items) ;
+        final ListView listview = (ListView) view.findViewById(R.id.listview) ;
+        listview.setAdapter(adapter) ;
+        // 선택된 곳에 따른 화면이동
+       // getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseRef = database.getReference("suggestion");
+
+        // Read from the database
+        databaseRef.child("suggestion").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                adapter.clear();
+                // 클래스 모델이 필요?
+                for (DataSnapshot notification : dataSnapshot.getChildren()) {
+                    //MyFiles filename = (MyFiles) fileSnapshot.getValue(MyFiles.class);
+                    //하위키들의 value를 어떻게 가져오느냐???
+                    String str = notification.child("suggestion").getValue(String.class);
+                    Log.i("TAG: value is ", str);
+                    items.add(str);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        // listview 생성 및 adapter 지정.
+        btn.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                rootNode = FirebaseDatabase.getInstance();
+                mDatabase = rootNode.getReference("suggestion");
+                //모든 값 가져오기
+                String suggestion= editText.getText().toString();
+                suggest suggestions = new suggest(suggestion);
+                mDatabase.child("suggestion").push().setValue(suggestions);
+                // 아이템 추가.
+                items.add(editText.getText().toString());
+                // listview 갱신
+                adapter.notifyDataSetChanged();
+                editText.getText().clear();
+            }
+        }) ;
+        return view;
     }
 }
